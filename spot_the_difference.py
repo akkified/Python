075376b011941b43b17cfb98f0c6cce44e15a5a4
akkified <115843366+akkified@users.noreply.github.com>
2023-04-13 11:52:58 -0400

    Create spot_the_difference.py

---
+++ spot_the_difference.py
@@ -0,0 +1,12 @@
+import pygame
+from time import sleep
+from random import randrange
+
+pygame.init()
+
+width = pygame.display.Info().current_w
+heigth = pygame.display.Info().current_h
+
+screen = pygame.display.set_mode((width, height))
+
+pygane.quit()
