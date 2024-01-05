57ba289d45d0c7c4295d73a4e19e80a86d806c7e
akkified <115843366+akkified@users.noreply.github.com>
2024-01-05 18:28:14 +0000

    working calc

--- calc.py
+++ calc.py
@@ -1,1 +1,14 @@
-int(input("What do you want to calculate?"))
+def calc(num1,num2,operation):
+    if operation == "Add":
+        print(num1 + num2)
+    elif operation == "Subtract":
+        print(num1 - num2)
+    elif operation == "Multiply":
+        print(num1 * num2)
+    elif operation == "Divide":
+        print(num1/num2)
+    else:
+        print("You entered the wrong operation you moron!")
+
+calc(1,3,Add)
+
