4af8b5c772450174410f0fb372da41b212a57742
akkified <115843366+akkified@users.noreply.github.com>
2024-01-05 18:28:15 +0000

    working calc

--- calc.py
+++ calc.py
@@ -1,14 +1,15 @@
-def calc(num1,num2,operation):
+def calc(num1,num2):
+    operation = input("What operation do want to do? ")
     if operation == "Add":
-        print(num1 + num2)
+        return(num1 + num2)
     elif operation == "Subtract":
-        print(num1 - num2)
+        return(num1 - num2)
     elif operation == "Multiply":
-        print(num1 * num2)
+        return(num1 * num2)
     elif operation == "Divide":
-        print(num1/num2)
+        return(num1/num2)
     else:
-        print("You entered the wrong operation you moron!")
+        print("You entered the wrong operation!")
 
-calc(1,3,Add)
+print(calc(float(input("What is your first number? ")),float(input("What is your second number? "))))
 
